c98382121b9a42db50668531ecfba5450a58cb4a
Jonathan Lopez <jonatlop@gmail.com>
2021-05-18 14:56:03 -0500

    cookie-session and jwt are added to the project and a jwt token in generated in the signup handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import mongoose from 'mongoose'
+import cookieSession from 'cookie-session'
 
 import { currentUserRouter } from './routes/current_user'
 import { signinRouter } from './routes/signin'
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error_handler'
 import { NotFoundError } from './errors/not_found_error'
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
