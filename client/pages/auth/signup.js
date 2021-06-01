0edb00c73414c059abaa2fd88d7dd345cfc1b8d4
Jonathan Lopez <jonatlop@gmail.com>
2021-05-31 21:47:57 -0500

    Added simple form and input handling in the signup component

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+
 export default () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+  
   return (
-    <form>
+    <form submit={}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input className="form-control" onChange={e => setEmail(e.target.value)} />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input className="form-control" type="password" />
+        <input
+          className="form-control"
+          type="password"
+          onChange={e => setPassword(e.target.value)} />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
