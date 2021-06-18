f41fc49659ef073dc8cc776704980805d25b9027
Jonathan Lopez <jonatlop@gmail.com>
2021-06-18 15:23:12 -0500

    Setup of authorization in the tests by generating a fake cookie

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,11 @@ import 'express-async-errors'
 import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
 
-import { errorHandler, NotFoundError } from '@jonatlop-ticketing/common'
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser
+} from '@jonatlop-ticketing/common'
 import { createTicketRouter } from './routes/new'
 
 const app = express()
@@ -15,7 +19,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 )
-
+app.use(currentUser)
 app.use(createTicketRouter)
 
 app.all('*', async () => {
