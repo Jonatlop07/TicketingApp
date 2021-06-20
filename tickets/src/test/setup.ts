9ebae5c347689a77ed210f5b849f2beb577aa4bf
Jonathan Lopez <jonatlop@gmail.com>
2021-06-19 21:20:17 -0500

    Setup of the show ticket tests and implementation of the route handler. Also a mongoose timeout problem is fixed by adding a property to jest object config in auth and tickets package.json

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,7 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
-import request from 'supertest'
-import { app } from '../app'
 import jwt from 'jsonwebtoken'
 
 declare global {
@@ -35,8 +33,8 @@ beforeEach(async () => {
 })
 
 afterAll(async () => {
-  await mongo.stop()
   await mongoose.connection.close()
+  await mongo.stop()
 })
 
 global.signin = () => {
