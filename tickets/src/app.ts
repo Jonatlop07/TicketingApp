875966a9ba6fea85eb6b66fed171989c11fca31a
Jonathan Lopez <jonatlop@gmail.com>
2021-06-19 21:33:56 -0500

    Setup the test getting all the tickets use case. Implemented its corresponding route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -10,6 +10,7 @@ import {
 } from '@jonatlop-ticketing/common'
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
 
 const app = express()
 app.set('trust proxy', true)
@@ -23,6 +24,7 @@ app.use(
 app.use(currentUser)
 app.use(createTicketRouter)
 app.use(showTicketRouter)
+app.use(indexTicketRouter)
 
 app.all('*', async () => {
   throw new NotFoundError()
