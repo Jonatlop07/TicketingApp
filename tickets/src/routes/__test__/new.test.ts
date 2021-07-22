e02f91c72f21b025f6be59de34b08360b9fa9782
Jonathan <jonatlop@gmail.com>
2021-07-21 21:27:03 -0500

    Removed yarn.lock in all the service directories

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
+import { natsWrapper } from '../../nats_wrapper'
 
 const title = 'Ticket'
 const price = 12.2
@@ -77,4 +78,16 @@ it('creates a ticket with valid inputs', async () => {
   expect(tickets.length).toEqual(1)
   expect(tickets[0].title).toEqual(title)
   expect(tickets[0].price).toEqual(price)
+})
+
+it('publishes an event', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price
+    }).expect(201)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
 })
